2ae52053eddf86cf718dc1e4e4c666609fc3c69e
Jan0660 <58996212+Jan0660@users.noreply.github.com>
2020-07-12 21:25:37 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,366 @@
+﻿namespace Minecraft_PixelArt_Maker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown4 = new System.Windows.Forms.NumericUpDown();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.progressBar2 = new System.Windows.Forms.ProgressBar();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox2.Location = new System.Drawing.Point(12, 158);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(292, 243);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox3.Location = new System.Drawing.Point(313, 86);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(383, 315);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Select folder";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Do the thing";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 70);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Save";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.DecimalPlaces = 2;
+            this.numericUpDown1.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.numericUpDown1.Location = new System.Drawing.Point(85, 97);
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            131072});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 6;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.DecimalPlaces = 2;
+            this.numericUpDown2.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.numericUpDown2.Location = new System.Drawing.Point(85, 71);
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            131072});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown2.TabIndex = 7;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.DecimalPlaces = 2;
+            this.numericUpDown3.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.numericUpDown3.Location = new System.Drawing.Point(85, 45);
+            this.numericUpDown3.Minimum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            131072});
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown3.TabIndex = 8;
+            this.numericUpDown3.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown4
+            // 
+            this.numericUpDown4.DecimalPlaces = 2;
+            this.numericUpDown4.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.numericUpDown4.Location = new System.Drawing.Point(85, 19);
+            this.numericUpDown4.Minimum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            131072});
+            this.numericUpDown4.Name = "numericUpDown4";
+            this.numericUpDown4.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown4.TabIndex = 9;
+            this.numericUpDown4.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.numericUpDown4);
+            this.groupBox1.Controls.Add(this.numericUpDown1);
+            this.groupBox1.Controls.Add(this.numericUpDown2);
+            this.groupBox1.Controls.Add(this.numericUpDown3);
+            this.groupBox1.Location = new System.Drawing.Point(93, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(211, 127);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Multipliers";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(7, 99);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(28, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Blue";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Green";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Red";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Transparency";
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.DefaultExt = "png";
+            this.saveFileDialog1.Title = "Save de imag";
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.RootFolder = System.Environment.SpecialFolder.MyComputer;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(313, 12);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(383, 23);
+            this.progressBar1.TabIndex = 11;
+            // 
+            // progressBar2
+            // 
+            this.progressBar2.Location = new System.Drawing.Point(313, 41);
+            this.progressBar2.Name = "progressBar2";
+            this.progressBar2.Size = new System.Drawing.Size(383, 23);
+            this.progressBar2.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(310, 70);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Finished";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(90, 142);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(113, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Color converted image";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "Images|*.png;*.jpg,*.jpeg,*.bmp,*.tiff\"";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(637, 70);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(59, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "u/Jan6006";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(708, 413);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.progressBar2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.NumericUpDown numericUpDown4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ProgressBar progressBar2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,207 @@
+﻿#define DEBUG
+using System;
+using System.Drawing.Imaging;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Minecraft_PixelArt_Maker
+{
+    public partial class Form1 : Form
+    {
+        static string WorkingFolder = Directory.GetCurrentDirectory() + "\\Blocks\\";
+        static Bitmap[] bitmaps = new Bitmap[1000];
+        static Color[] colours = new Color[1000];
+        static int BitMapCount = 0;
+        static Decimal mult_A;
+        static Decimal mult_R;
+        static Decimal mult_G;
+        static Decimal mult_B;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            //MessageBox.Show(folderBrowserDialog1.SelectedPath);
+            WorkingFolder = folderBrowserDialog1.SelectedPath;
+        }
+        static void GetReady()
+        {
+
+            Array.Resize(ref bitmaps, 1000);
+            Array.Resize(ref colours, 1000);
+            BitMapCount = 0;
+            foreach (string file in Directory.GetFiles(WorkingFolder))
+            {
+                bitmaps[BitMapCount] = new Bitmap(file);
+                BitMapCount++;
+            }
+            Array.Resize(ref bitmaps, BitMapCount);
+            Array.Resize(ref colours, BitMapCount);
+            Console.WriteLine(bitmaps.Length + " Bitmaps");
+            int i = 0;
+            Bitmap[] bitboi = bitmaps;
+            foreach (Bitmap bitmap in bitboi)
+            {
+                colours[i] = GetAverageColor(bitmap);
+                i++;
+            }
+        }
+
+        static Color GetAverageColor(Bitmap image)
+        {
+            decimal alpha = 0, red = 0, green = 0, blue = 0, pixuls = 0;
+            Bitmap img = image;
+            for (int i = 0; i < img.Width; i++)
+            {
+                Console.WriteLine(i);
+                for (int j = 0; j < img.Height; j++)
+                {
+                    /*i = width, j = height */
+                    Color pixel = img.GetPixel(i, j);
+                    alpha += pixel.A;
+                    red += pixel.R;
+                    green += pixel.G;
+                    blue += pixel.B;
+                    pixuls++;
+                }
+            }
+            Color average = Color.FromArgb(Convert.ToInt32(alpha / pixuls), Convert.ToInt32(red / pixuls), Convert.ToInt32(green / pixuls), Convert.ToInt32(blue / pixuls));
+            return average;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            mult_A = numericUpDown4.Value;
+            mult_R = numericUpDown3.Value;
+            mult_G = numericUpDown2.Value;
+            mult_B = numericUpDown1.Value;
+            Thread thread = new Thread(DoTheThing);
+            thread.SetApartmentState(ApartmentState.STA);
+            thread.Start();
+        }
+        public void DoTheThing()
+        {
+            GetReady();
+            string selectedPath = "";
+            OpenFileDialog saveFileDialog1 = new OpenFileDialog();
+            saveFileDialog1.Filter = "Images|*.png;*.jpg;*.jpeg;*.bmp;*.tiff";
+            saveFileDialog1.RestoreDirectory = true;
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                selectedPath = saveFileDialog1.FileName;
+            }
+            Bitmap imge = new Bitmap(selectedPath);
+            Bitmap idk = new Bitmap(imge.Width, imge.Height, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+            if (progressBar1.InvokeRequired)
+            {
+                progressBar1.Invoke(new MethodInvoker(delegate { progressBar1.Maximum = imge.Width; }));
+            }
+            for (int i = 0; i < imge.Width; i++)
+            {
+                for (int j = 0; j < imge.Height; j++)
+                {
+                    /*i = width, j = height*/
+                    idk.SetPixel(i, j, Method1(imge.GetPixel(i, j)));
+                }
+                progressBar1.Invoke(new MethodInvoker(delegate { progressBar1.Value = i + 1; }));
+            }
+            //stage 2
+            //winforms cross thread shit
+            if (pictureBox2.InvokeRequired)
+            {
+                pictureBox2.Invoke(new MethodInvoker(delegate { pictureBox2.BackgroundImage = idk; progressBar2.Maximum = idk.Width; }));
+            }
+            Bitmap res = new Bitmap(idk.Width * 16, idk.Height * 16, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+            for (int i = 0; i < idk.Width; i++)
+            {
+                for (int j = 0; j < idk.Height; j++)
+                {
+                    /*i = width, j = height*/
+                    int ind = 0;
+                    foreach (Color color in colours)
+                    {
+                        if (idk.GetPixel(i, j) == color)
+                        {
+                            Point point = new Point(i * 16, j * 16);
+                            Graphics g = Graphics.FromImage(res);
+                            g.DrawImage(bitmaps[ind], point);
+                            g.Dispose();
+                        }
+                        ind++;
+                    }
+                }
+                progressBar2.Invoke(new MethodInvoker(delegate { progressBar2.Value = i + 1; }));
+            }
+            if (pictureBox3.InvokeRequired)
+            {
+                pictureBox3.Invoke(new MethodInvoker(delegate { pictureBox3.BackgroundImage = res; }));
+            }
+        }
+        static Color Method1(Color input)
+        {
+            int a = input.A;
+            int r = input.R;
+            int g = input.G;
+            int b = input.B;
+            int i = 0;
+            Decimal away_a;
+            Decimal away_r;
+            Decimal away_g;
+            Decimal away_b;
+            Decimal away;
+            Decimal BestAway = 2949000;
+            Color BestMatch = Color.Black;
+            foreach (Color color in colours)
+            {
+                away_a = a - color.A;
+                if (away_a < 0) { away_a *= -1; }
+                away_a *= mult_A;
+                away_r = r - color.R;
+                if (away_r < 0) { away_r *= -1; }
+                away_r *= mult_R;
+                away_g = g - color.G;
+                if (away_g < 0) { away_g *= -1; }
+                away_g *= mult_G;
+                away_b = b - color.B;
+                if (away_b < 0) { away_b *= -1; }
+                away_b *= mult_B;
+                away = 0;
+                away = away_a;
+                away += away_r + away_g + away_b;
+                if (away < 0)
+                {
+                    away *= -1;
+                }
+                if (away < BestAway)
+                {
+                    //Console.WriteLine("New best match, Last away" + BestAway + ", New away " + away);
+                    //Console.WriteLine("old color =" + BestMatch.ToString() + ", New color =" + color.ToString());
+                    BestAway = away;
+                    BestMatch = color;
+                }
+                i++;
+            }
+            return BestMatch;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.ShowDialog();
+            Image save = pictureBox3.BackgroundImage;
+            save.Save(saveFileDialog1.FileName);
+        }
+    }
+}
